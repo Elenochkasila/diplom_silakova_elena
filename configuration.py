97f53f128ec862bbb8aae41aca31a6e6ac1cbff6
Elenochkasila <133567536+Elenochkasila@users.noreply.github.com>
2023-07-19 17:38:08 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL = "https://a311fa03-cad1-4f11-abae-360f4c9cd3e3.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,16 @@
+headers = {
+    "Content-Type": "application/json"
+}
+user_body = {
+    "firstName": "Alena",
+    "lastName": "Silakova",
+    "address": "Moscow",
+    "metroStation": 25,
+    "phone": "+79991112211",
+    "rentTime": 3,
+    "deliveryDate": "2023-07-25",
+    "comment": "sms",
+    "color": [
+        "BLACK"
+    ]
+}
